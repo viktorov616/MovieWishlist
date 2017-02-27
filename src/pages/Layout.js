1351660e5751661301251351e85a4a376bea98bc
Viktorov616 <viktorov616@gmail.com>
2017-02-27 20:09:00 +0300

    Add navigation.

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,14 +1,31 @@
 import React, { PropTypes } from 'react';
 
+import Nav from '../components/Nav';
+
 export default function Layout(props) {
+  const nav = [{
+    linkPath: '/',
+    linkName: 'Wishlist',
+  }, {
+    linkPath: '/search',
+    linkName: 'Search',
+  }];
+
   return (
     <div className="layout">
+      <Nav
+        nav={nav}
+        currentPage={props.location.pathname}
+      />
       { React.cloneElement(props.children, { ...props }) }
     </div>
   );
 }
 
 Layout.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
   children: PropTypes.node,
 };
 
