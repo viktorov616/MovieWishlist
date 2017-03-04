809f44cd8249d90587014c6a6ce60e52869a7632
Viktorov616 <viktorov616@gmail.com>
2017-03-04 14:24:36 +0300

    Add localStorage functional.

--- src/reducers/wishlist.js
+++ src/reducers/wishlist.js
@@ -1,15 +1,22 @@
+const movies = JSON.parse(localStorage.getItem('movies')) || [];
 const defaultState = {
-  movies: [],
+  movies,
 };
 
 export default function wishlist(state = defaultState, action) {
   switch (action.type) {
-    case 'ADD_TO_WISHLIST':
-      return Object.assign({}, state, { movies: state.movies.concat(action.movie) });
-    case 'DELETE_FROM_WISHLIST':
+    case 'ADD_TO_WISHLIST': {
+      const newMovies = state.movies.concat(action.movie);
+      localStorage.setItem('movies', JSON.stringify(newMovies));
+      return Object.assign({}, state, { movies: newMovies });
+    }
+    case 'DELETE_FROM_WISHLIST': {
+      const newMovies = state.movies.filter(movie => movie.imdbID !== action.id);
+      localStorage.setItem('movies', JSON.stringify(newMovies));
       return Object.assign({}, state, {
-        movies: state.movies.filter(movie => movie.imdbID !== action.id),
+        movies: newMovies,
       });
+    }
     default:
       return state;
   }
