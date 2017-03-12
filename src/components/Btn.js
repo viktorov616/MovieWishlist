fa79ad38f8e4a6298ed54acfaa57bbf84f621c42
Viktorov616 <viktorov616@gmail.com>
2017-03-12 23:37:55 +0300

    Add tablet styles. Add color animations.

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,33 +1,21 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
-export default class Btn extends Component {
-  componentWillUpdate(nextProps) {
-    if (this.props.text !== nextProps.text) {
-      const { height } = getComputedStyle(this.btn);
-      this.btn.style.maxHeight = `${parseInt(height, 10) + 1000}px`;
+export default function Btn(props) {
+  const btnDefaultClass = 'btn';
+  const btnClass = props.mods.reduce(
+    (result, mod) => result.concat(`${btnDefaultClass}--${mod}`), [btnDefaultClass],
+  ).join(' ');
 
-      console.log(height);
-    }
-  }
-
-  render() {
-    const { props } = this;
-    const btnDefaultClass = 'btn';
-    const btnClass = props.mods.reduce(
-      (result, mod) => result.concat(`${btnDefaultClass}--${mod}`), [btnDefaultClass],
-    ).join(' ');
-
-    return (
-      <button
-        onClick={props.onClick}
-        className={btnClass}
-        ref={btn => (this.btn = btn)}
-        type={props.type}
-      >
-        { props.text }
-      </button>
-    );
-  }
+  return (
+    <button
+      onClick={props.onClick}
+      className={btnClass}
+      ref={btn => (this.btn = btn)}
+      type={props.type}
+    >
+      { props.text }
+    </button>
+  );
 }
 
 Btn.propTypes = {
