b539fe4cae64d1a4659bfbf6e682c3f4ee32639e
Viktorov616 <viktorov616@gmail.com>
2017-03-25 23:52:19 +0300

    Fix production store.

--- src/store.prod.js
+++ src/store.prod.js
@@ -1,10 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import rootReducer from './reducers/index';
 
+const enhancers = compose(
+  applyMiddleware(thunk),
+);
 const defaultState = {};
-const store = createStore(rootReducer, defaultState);
+const store = createStore(rootReducer, defaultState, enhancers);
 export const history = syncHistoryWithStore(browserHistory, store);
 
 export default store;
